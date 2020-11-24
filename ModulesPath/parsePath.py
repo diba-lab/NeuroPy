ef1fd148411a6385ce64dfe58bc757893d4f894b
Nat Kinsky <kinsky@bu.edu>
2020-11-24 15:45:48 -0500

    actually added windows compatability

--- ModulesPath/parsePath.py
+++ ModulesPath/parsePath.py
@@ -36,7 +36,7 @@ class Recinfo:
     """
 
     def __init__(self, basePath):
-        self.basePath = Path(basePath).as_posix()
+        self.basePath = Path(basePath)
 
         filePrefix = None
         for file in os.listdir(basePath):
@@ -195,7 +195,7 @@ class recfiles:
 
 class sessionname:
     def __init__(self, f_prefix):
-        basePath = str(f_prefix.parent)
+        basePath = str(f_prefix.parent.as_posix())
         self.sessionName = basePath.split("/")[-2] + basePath.split("/")[-1]
         self.name = basePath.split("/")[-2]
         self.day = basePath.split("/")[-1]
