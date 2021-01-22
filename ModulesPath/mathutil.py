f369f2a704b78f3b04ac444ec4560a4f633327da
bapungiri <bapun.k.giri@gmail.com>
2021-01-21 23:16:46 -0500

    eventpsth moved to mathutil as function

--- ModulesPath/mathutil.py
+++ ModulesPath/mathutil.py
@@ -4,6 +4,7 @@ import pandas as pd
 from sklearn.decomposition import FastICA, PCA
 from scipy import stats
 from hmmlearn.hmm import GaussianHMM
+from ccg import correlograms
 
 
 def partialcorr(x, y, z):
@@ -215,3 +216,67 @@ def hmmfit1d(Data, n_comp=2, n_iter=50):
         hmmlabels = relabeled_states
 
     return hmmlabels
+
+
+def eventpsth(ref, event, fs, quantparam=None, binsize=0.01, window=1, nQuantiles=1):
+    """psth of 'event' with respect to 'ref'
+
+    Parameters
+    ----------
+    ref (array):
+        1-D array of timings of reference event in seconds
+    event (1D array):
+        timings of events whose psth will be calculated
+    fs:
+        sampling rate
+    quantparam (1D array):
+        values used to divide 'ref' into quantiles
+    binsize (float, optional):
+        [description]. Defaults to 0.01.
+    window (int, optional):
+        [description]. Defaults to 1.
+    nQuantiles (int, optional):
+        [description]. Defaults to 10.
+
+    Returns
+    -------
+        [type]: [description]
+    """
+
+    ref = np.asarray(ref)
+    event = np.asarray(event)
+
+    if quantparam is not None:
+        assert len(event) == len(quantparam), print(
+            "length of quantparam must be same as ref events"
+        )
+        quantiles = pd.qcut(quantparam, nQuantiles, labels=False)
+
+        quants, eventid = [], []
+        for category in range(nQuantiles):
+            indx = np.where(quantiles == category)[0]
+            quants.append(ref[indx])
+            eventid.append(category * np.ones(len(indx)).astype(int))
+
+        quants.append(event)
+        eventid.append(((nQuantiles + 1) * np.ones(len(event))).astype(int))
+
+        quants = np.concatenate(quants)
+        eventid = np.concatenate(eventid)
+    else:
+        quants = np.concatenate((ref, event))
+        eventid = np.concatenate([np.ones(len(ref)), 2 * np.ones(len(event))]).astype(
+            int
+        )
+
+    sort_ind = np.argsort(quants)
+
+    ccg = correlograms(
+        quants[sort_ind],
+        eventid[sort_ind],
+        sample_rate=fs,
+        bin_size=binsize,
+        window_size=window,
+    )
+
+    return ccg[:-1, -1, :]
