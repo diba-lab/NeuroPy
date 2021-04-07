335e08968c42b48e945cf233523509206d5280e7
nkinsky <nkinsky@umich.edu>
2021-04-07 16:11:57 -0400

    added/moved files

--- examples/Cython_testing/setup.py
+++ examples/Cython_testing/setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 from Cython.Build import cythonize
 import numpy
 
-setup(ext_modules = cythonize(["convolve3.pyx"]), 
+setup(ext_modules = cythonize(["ccg_py.py"]), 
       include_dirs=[numpy.get_include()])
