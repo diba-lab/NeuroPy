970034e00c9fd9c1fc3f9f09a694b953c58d6aef
bapungiri <bapun.k.giri@gmail.com>
2021-06-02 17:35:44 -0400

    event and spiketrain class

---
+++ ModulesPath/core/event.py
@@ -0,0 +1,16 @@
+import numpy as np
+from .datawriter import DataWriter
+
+
+class Event(DataWriter):
+    def __init__(self, times=None, labels=None) -> None:
+        pass
+
+    def to_dataframe(self):
+        pass
+
+    def add_event(self):
+        pass
+
+    def remove_event(self):
+        pass
