710e67dfc85f33388bdf9cd663e6e04a9922c99b
bapungiri <bapun.k.giri@gmail.com>
2021-06-07 14:06:57 -0400

    New classes used

--- ModulesPath/callfunc.py
+++ ModulesPath/callfunc.py
@@ -1,7 +1,7 @@
-from .artifactDetect import findartifact
+from .artifactDetect import Artifact
 from .behavior import Paradigm
 from .decoders import DecodeBehav
-from .getPosition import ExtractPosition
+from .getPosition import SessPosition, SessTrack
 from .getSpikes import Spikes, SessNeurons
 from .lfpEvent import Hswa, Ripple, Spindle, Theta, Gamma
 from .parsePath import Recinfo
@@ -12,7 +12,6 @@ from .sleepDetect import SleepScore
 from .spkEvent import PBE, LocalSleep
 from .viewerData import SessView
 from .track import Track
-from .getPosition import SessPosition, SessTrack
 import pickle
 
 
@@ -21,12 +20,10 @@ class processData:
         """Make sure to enter in the tracking scale factor if you have used a properly sized wand to optitrack calibration"""
         self.recinfo = Recinfo(basepath)
 
-        self.position = ExtractPosition(self.recinfo)
-        self.tracks = Track(self.recinfo)
-        self.pos = SessPosition(self.recinfo)
-        self.tracku = SessTrack(basepath=self.recinfo, position=self.pos)
+        self.position = SessPosition(self.recinfo)
+        self.track = SessTrack(basepath=self.recinfo, position=self.pos)
         self.paradigm = Paradigm(self.recinfo)
-        self.artifact = findartifact(self.recinfo)
+        self.artifact = Artifact(self.recinfo)
         self.utils = SessionUtil(self.recinfo)
 
         self.spikes = Spikes(self.recinfo)
