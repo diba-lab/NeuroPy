7233edb28efc91078afb70d53350ac71e7a8019c
bapungiri <bapun.k.giri@gmail.com>
2021-06-09 10:38:57 -0400

    added paradigm

--- ModulesPath/sessobj/__init__.py
+++ ModulesPath/sessobj/__init__.py
@@ -2,3 +2,4 @@ from .oscillations import Hswa, Theta, Spindle, Gamma, Ripple
 from .neurons import SessNeurons
 from .position import SessPosition, SessTrack
 from .artifact import Artifact
+from .paradigm import Paradigm
