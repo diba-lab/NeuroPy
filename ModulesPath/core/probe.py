697d6990f40577f0f024338b5e10b9dd3f09b4a8
bapungiri <bapun.k.giri@gmail.com>
2021-06-16 18:15:08 -0400

    fixed probe

--- ModulesPath/core/probe.py
+++ ModulesPath/core/probe.py
@@ -29,27 +29,49 @@ class Shank:
 
         self.x = positions[:, 0]
         self.y = positions[:, 1]
+        self.connected = np.ones(np.sum(contacts_per_column), dtype=bool)
 
         if channel_id is None:
             self.channel_id = np.arange(np.sum(contacts_per_column))
+        else:
+            self.channel_id = channel_id
+
+    @property
+    def channel_id(self):
+        return self._channel_id
+
+    @channel_id.setter
+    def channel_id(self, chan_ids):
+        assert self.n_contacts == len(chan_ids)
+        self._channel_id = chan_ids
 
     @property
     def n_contacts(self):
         return len(self.x)
 
     def to_dict(self):
-        layout = {"x": self.x, "y": self.y, "channel_id": self.channel_id}
+        layout = {
+            "x": self.x,
+            "y": self.y,
+            "channel_id": self.channel_id,
+            "connected": self.connected,
+        }
         return layout
+
+    def set_disconnected(self, channel_ids):
+        self.connected[np.isin(self.channel_id, channel_ids)] = False
 
     def to_dataframe(self):
         return pd.DataFrame(self.to_dict())
 
-    def move(self):
-        pass
+    def move(self, translation):
+        x, y = translation
+        self.x += x
+        self.y += y
 
 
 class Probe:
-    def __init__(self, shanks) -> None:
+    def __init__(self, shanks, shank_pitch=(150, 0)) -> None:
 
         if isinstance(shanks, Shank):
             shanks = [shanks]
@@ -57,79 +79,143 @@ class Probe:
         if isinstance(shanks, list):
             assert np.all([_.__class__.__name__ == "Shank" for _ in shanks])
 
-        self.shanks: list[Shank] = shanks
+        self._data = pd.DataFrame(
+            columns=["x", "y", "channel_id", "connected", "shank_id"]
+        )
+        x = np.arange(len(shanks)) * shank_pitch[0]
+        y = np.arange(len(shanks)) * shank_pitch[1]
+        for i, shank in enumerate(shanks):
+            shank_df = shank.to_dataframe()
+            shank_df["x"] += x[i]
+            shank_df["y"] += y[i]
+            shank_df["shank_id"] = i * np.ones(shank.n_contacts)
+            self._data = self._data.append(shank_df)
+
+    @property
+    def n_contacts(self):
+        return len(self._data)
 
     @property
     def n_shanks(self):
-        return len(self.shanks)
+        return np.max(self._data["shank_id"]) + 1
 
     @property
     def shank_id(self):
-        shank_id = []
-        for n, shank in enumerate(self.shanks):
-            shank_id.append([n] * shank.n_contacts)
-
-        return np.concatenate(shank_id)
+        return self._data["shank_id"].values
 
     @property
     def x(self):
-        x = []
-        for shank in self.shanks:
-            x.append(shank.x)
+        return self._data["x"].values
 
-        return np.concatenate(x)
+    @property
+    def x_max(self):
+        return np.max(self._data["x"].values)
 
     @property
     def y(self):
-        y = []
-        for shank in self.shanks:
-            y.append(shank.y)
-
-        return np.concatenate(y)
+        return self._data["y"].values
 
     @property
     def channel_id(self):
-        y = []
-        for shank in self.shanks:
-            y.append(shank.y)
+        return self._data["channel_id"].values
 
-        return np.concatenate(y)
+    @property
+    def connected(self):
+        return self._data["connected"].values
 
-    def add_shank(self, shank: Shank, shank_pitch=(150, 0)):
-        self.shanks.append(shank)
+    def add_shank(self, shank: Shank):
+        shank_df = shank.to_dataframe()
+        shank_df["shank_id"] = (self.n_shanks - 1) * np.ones(shank.n_contacts)
+        self._data = self._data.append(shank_df)
 
     def to_dict(self):
-        layout = {
-            "x": self.x,
-            "y": self.y,
-            "channel_id": self.channel_id,
-            "shank_id": self.shank_id,
-        }
-        return layout
+        return self._data.to_dict()
 
     def to_dataframe(self):
-        return pd.DataFrame(self.to_dict())
+        return self._data
 
-    def move(self):
-        pass
+    def move(self, translation):
+        x, y = translation
+        self._data["x"] += x
+        self._data["y"] += y
 
 
 class ProbeGroup(DataWriter):
     def __init__(self, filename=None) -> None:
 
         super().__init__(filename=filename)
-        self.probes: list[Probe] = []
+        self._data = pd.DataFrame(
+            {
+                "x": np.array([]),
+                "y": np.array([]),
+                "channel_id": np.array([]),
+                "shank_id": np.array([]),
+                "connected": np.array([], dtype=bool),
+                "probe_id": np.array([]),
+            }
+        )
+        self.metadata = {}
+        self.load()
+
+    def load(self):
+        if self.filename is not None:
+            data = super().load()
+            if data is not None:
+                self._data, self.metadata = data["probemap"], data["metadata"]
+
+    @property
+    def x(self):
+        return self._data["x"].values
+
+    @property
+    def y(self):
+        return self._data["y"].values
+
+    @property
+    def n_contacts(self):
+        return len(self._data)
+
+    @property
+    def channel_id(self):
+        return self._data["channel_id"].values
+
+    @property
+    def shank_id(self):
+        return self._data["shank_id"].values
+
+    @property
+    def probe_id(self):
+        return self._data["probe_id"].values
 
     @property
     def n_probes(self):
-        return len(self.probes)
+        return len(np.unique(self.probe_id))
 
     def n_shanks(self):
+        return np.sum(np.unique(self.shank_id, return_counts=True))
 
-        return np.sum([probe.n_shanks for probe in self.probes])
+    @property
+    def get_disconnected(self):
+        return self._data[self._data["connected"] == False]
 
-    def add_probe(self, probe_pitch=(500, 0)):
-        pass
+    def add_probe(self, probe: Probe):
+        probe_df = probe.to_dataframe()
+        probe_df["probe_id"] = self.n_probes * np.ones(probe.n_contacts)
+        self._data = self._data.append(probe_df)
+
+    def to_dict(self):
+        return self._data
+
+    def from_dict(self, d: dict):
+        if d is not None:
+            self._data = d
 
     def to_dataframe(self):
-        pass
+        return pd.DataFrame(self._data)
+
+    def remove_probes(self, probe_id=None):
+        self.probes = []
+
+    def save(self):
+        data = self.to_dict()
+        return super().save(data)
