fcd6e77eda116570efe21dd59e9f568c18a8e792
bapungiri <bapun.k.giri@gmail.com>
2021-06-26 22:56:53 -0400

    basics raster plot working

--- neuropy/plotting/spikes.py
+++ neuropy/plotting/spikes.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 from .. import core
 import numpy as np
 
@@ -6,9 +7,7 @@ import numpy as np
 def plot_raster(
     neurons: core.Neurons,
     ax=None,
-    period=None,
     sort_by_frate=False,
-    tstart=0,
     color=None,
     marker="|",
     markersize=2,
@@ -18,7 +17,7 @@ def plot_raster(
 
     Parameters
     ----------
-    spikes : list, optional
+    neurons : list, optional
         Each array within list represents spike times of that unit, by default None
     ax : obj, optional
         axis to plot onto, by default None
@@ -40,71 +39,33 @@ def plot_raster(
     if ax is None:
         fig, ax = plt.subplots()
 
-    if spikes is None:
-        pyr = self.pyr
-        intneur = self.intneur
-        mua = self.mua
-        spikes = mua + pyr + intneur
+    n_neurons = neurons.n_neurons
 
-        color = (
-            [self.colors["mua"]] * len(mua)
-            + [self.colors["pyr"]] * len(pyr)
-            + [self.colors["intneur"]] * len(intneur)
-        )
+    if color is None:
+        color = ["#2d3143"] * n_neurons
+    elif isinstance(color, str):
+        try:
+            cmap = mpl.cm.get_cmap(color)
+            color = [cmap(_ / n_neurons) for _ in range(n_neurons)]
+        except:
+            color = [color] * n_neurons
 
-        # --- mimics legend for labeling unit category ---------
-        y = 0.5
-        for cell in self.colors:
-            ax.annotate(
-                cell,
-                xy=(0.9, y),
-                xycoords="figure fraction",
-                color=self.colors[cell],
-            )
-            y -= 0.05
-    else:
-        assert isinstance(spikes, list), "Please provide a list of arrays"
-        if color is None:
-            color = ["#2d3143"] * len(spikes)
-        elif isinstance(color, str):
-
-            try:
-                cmap = mpl.cm.get_cmap(color)
-                color = [cmap(_ / len(spikes)) for _ in range(len(spikes))]
-            except:
-                color = [color] * len(spikes)
-
-    # print(f"Plotting {len(spikes)} cells")
-    frate = [len(cell) for cell in spikes]  # number of spikes ~= frate
-
-    if period is not None:
-        period_duration = np.diff(period)
-        spikes = [
-            cell[np.where((cell > period[0]) & (cell < period[1]))[0]]
-            for cell in spikes
-        ]
-        frate = np.asarray([len(cell) / period_duration for cell in spikes]).squeeze()
-
-    if sort_by_frate:
-        sort_frate_indices = np.argsort(frate)
-        spikes = [spikes[indx] for indx in sort_frate_indices]
-
-    for cell, spk in enumerate(spikes):
+    for ind, spiketrain in enumerate(neurons.spiketrains):
         if add_vert_jitter:
-            jitter_add = np.random.randn(len(spk)) * 0.1
+            jitter_add = np.random.randn(len(spiketrain)) * 0.1
             alpha_use = 0.25
         else:
             jitter_add, alpha_use = 0, 0.5
         ax.plot(
-            spk - tstart,
-            (cell + 1) * np.ones(len(spk)) + jitter_add,
+            spiketrain,
+            (ind + 1) * np.ones(len(spiketrain)) + jitter_add,
             marker,
             markersize=markersize,
-            color=color[cell],
+            color=color[ind],
             alpha=alpha_use,
         )
 
-    ax.set_ylim([0.5, len(spikes) + 0.5])
+    ax.set_xlim([neurons.t_start, neurons.t_stop])
     ax.set_xlabel("Time (s)")
     ax.set_ylabel("Units")
 
