69cd4908ef65485127bf43c9813ff77f5141bfd3
bapungiri <bapun.k.giri@gmail.com>
2021-07-03 13:32:28 -0400

    animal class updated

--- neuropy/core/animal.py
+++ neuropy/core/animal.py
@@ -6,77 +6,45 @@ from dataclasses import dataclass
 
 @dataclass
 class Animal(DataWriter):
-    name = None
-    alias = None
-    tag = None
-    sex = None
-    day = None
+    filename: Path = None
+    name: str = None
+    alias: str = None
+    tag: str = None
+    sex: str = None
+    day = None  # day of experiment
     date = None  # date of experiment
-    experimenter = None
-    weight = None
-    track = None
-    brain_region = None
+    experimenter: str = None  # Experimenter's name
+    experiment: str = None  # Experiments name
+    weight = None  # weight of the animal during the recording session
+    track = None  # what type track was used
+    brain_region = None  # which brain regions were implanted
     Notes: dict = {}
 
-    # def __init__(self) -> None:
-    #     """
-    #     Name:  name of the animal e.g. Roy, Ted, Kevin
-    #     Alias:  code names as per lab standard e.g, RatN, Rat3254,
-    #     Tag:  helps identifying category for analyses, e.g, sd, nsd, control, rollipramSD
-    #     Sex: sex of animal
-    #     Experimenter: Experimenter's name
-    #     weight  : wieght of the animal during this recording
-    #     Probe :  what probes were used
-    #     BrainRegion :  which brain regions were implanted
-    #     Day  : The number of experiments : Day1, Day2
-    #     Date :  Date of experiment
-    #     Experiment  : any name for experiment
-    #     Track  : type of maze used, linear, l-shpaed
-    #     files :
-    #     StartTime : startime of the experiment in this format, 2019-10-11_03-58-54
-    #     nFrames :
-    #     deletedStart (minutes):  deleted from raw data to eliminate noise, required       for synching video tracking
-    #     deletedEnd (minutes): deleted tiem from raw data
-    #     Notes:  Any important notes
+    def __post_init__(self):
+        super().__init__(filename=self.filename)
 
-    #     """
+    def to_dataframe(self):
+        return pd.DataFrame(self.to_dict())
 
-    #     if metadatafile.is_file():
-    #         metadata = pd.read_csv(metadatafile)
-    #         self.name = metadata.Name[0]
-    #         self.alias = metadata.Alias[0]
-    #         self.tag = metadata.Tag[0]
-    #         self.sex = metadata.Sex[0]
-    #         self.day = metadata.Day[0]
-    #         self.date = metadata.Date[0]
-
-    #     else:
-    #         metadata = pd.DataFrame(
-    #             columns=[
-    #                 "Name",
-    #                 "Alias",
-    #                 "Tag",
-    #                 "Sex",
-    #                 "Experimenter",
-    #                 "weight",
-    #                 "Probe",
-    #                 "BrainRegion",
-    #                 "Day",
-    #                 "Date",
-    #                 "Experiment",
-    #                 "Track",
-    #                 "files",
-    #                 "StartTime",
-    #                 "nFrames",
-    #                 "deletedStart (minutes)",
-    #                 "deletedEnd (minutes)",
-    #                 "Notes",
-    #             ]
-    #         )
-    #         metadata.to_csv(metadatafile)
-    #         print(
-    #             f"Template metadata file {metadatafile.name} created. Please fill it accordingly."
-    #         )
+    def write_csv(self):
+        pass
 
     def to_dict(self):
-        return
+        return {
+            "name": self.name,
+            "alias": self.alias,
+            "tag": self.tag,
+            "sex": self.sex,
+            "weight": self.weight,
+            "experimenter": self.experimenter,
+            "experiment": self.experiment,
+            "day": self.day,
+            "date": self.date,
+            "probe": self.probe,
+            "track": self.track,
+            "brain_region": self.brain_region,
+        }
+
+    @staticmethod
+    def from_dict(d):
+        pass
