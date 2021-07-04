fce2e52950c7b757d01dc3a19e0972be91158755
bapungiri <bapun.k.giri@gmail.com>
2021-07-04 10:45:58 -0400

    init imports

--- neuropy/core/__init__.py
+++ neuropy/core/__init__.py
@@ -1,7 +1,7 @@
 from .epoch import Epoch
 from .position import Position
 from .datawriter import DataWriter
-from .neurons import Neurons
+from .neurons import Neurons, BinnedSpiketrain, Mua
 from .probe import Shank, Probe, ProbeGroup
 from .signal import Signal
 from .ratemap import Ratemap
