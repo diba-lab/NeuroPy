17400ebe39e077e38572591fe849b6d42c93a0bd
bapungiri <bapun.k.giri@gmail.com>
2021-08-11 14:54:14 -0400

    probe plotting enh

--- neuropy/plotting/probe.py
+++ neuropy/plotting/probe.py
@@ -2,13 +2,26 @@ import matplotlib.pyplot as plt
 from ..core import ProbeGroup
 
 
-def plot_probe(probe: ProbeGroup, channel_id=True, disconnected=True, ax=None):
+def plot_probe(
+    probe: ProbeGroup,
+    annotate_channels=None,
+    channel_id=True,
+    disconnected=True,
+    ax=None,
+):
 
     if ax is None:
         _, ax = plt.subplots()
 
     ax.scatter(
-        probe.x, probe.y, s=16, marker="o", color="gray", zorder=1, linewidths=0.5
+        probe.x,
+        probe.y,
+        s=12,
+        marker="o",
+        color="gray",
+        zorder=1,
+        linewidths=0.5,
+        alpha=0.5,
     )
     if channel_id:
         for x, y, chan_id in zip(probe.x, probe.y, probe.channel_id):
@@ -16,8 +29,21 @@ def plot_probe(probe: ProbeGroup, channel_id=True, disconnected=True, ax=None):
 
     if disconnected:
         ax.scatter(
-            probe.get_disconnected.x, probe.get_disconnected.y, s=15, color="#FF5252"
+            probe.get_disconnected.x,
+            probe.get_disconnected.y,
+            s=18,
+            edgecolors="#FF5252",
+            facecolors="none",
         )
+
+    if annotate_channels is not None:
+        prb_data = probe.to_dataframe().set_index("channel_id")
+        for channel in annotate_channels:
+            x, y = (
+                prb_data.loc[[channel]].x.values[0],
+                prb_data.loc[[channel]].y.values[0],
+            )
+            ax.scatter(x, y, s=30, edgecolors="g", facecolors="none", linewidths=2)
 
     # ax.axhline(probe.y_max + 10, probe.x_min, probe.x_max, lw=4)
     ax.axis("off")
