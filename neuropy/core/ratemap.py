4623b874f5220fb8ae882836d88d21d61626e3f5
bapungiri <bapun.k.giri@gmail.com>
2021-09-29 17:33:29 -0400

    ratemap plotting enh

--- neuropy/core/ratemap.py
+++ neuropy/core/ratemap.py
@@ -9,18 +9,19 @@ class Ratemap:
         ybin=None,
         neuron_ids=None,
     ) -> None:
-        self.tuning_curves = tuning_curves
+        self.tuning_curves = np.asarray(tuning_curves)
         self.neuron_ids = neuron_ids
         self.xbin = xbin
         self.ybin = ybin
 
-    def calculate(self):
-        pass
-
     @property
     def xbin_centers(self):
-        pass
+        return self.xbin[:-1] + np.diff(self.xbin) / 2
 
     @property
     def ybin_centers(self):
-        pass
+        return self.ybin[:-1] + np.diff(self.ybin) / 2
+
+    @property
+    def n_neurons(self):
+        return self.tuning_curves.shape[0]
