b79202ad67b941a8331589d21bcef406fc56ebe5
nkinsky <nkinsky@umich.edu>
2021-10-05 11:46:53 -0400

    alignment progress

--- neuropy/io/miniscopeio.py
+++ neuropy/io/miniscopeio.py
@@ -6,41 +6,82 @@ import json
 
 
 class MiniscopeIO:
-    def __init__(self) -> None:
+    def __init__(self, basedir) -> None:
+        self.basedir = Path(basedir)
         pass
 
-    def load_timestamps(self, rec_folder, corrupted_videos=None):
-        timestamps, rec_metadata, vid_metadata, rec_start = load_timestamps(
-            rec_folder, corrupted_videos=corrupted_videos
-        )
+    def load_minian(self, minian_folder=None):
 
-    def load_multiple_timestamps(self, basedir):
-        """Loads multiple timestamps from"""
-        pass
+        # Try to autodetect minian folder
+        if minian_folder is None:
+            assert (
+                len(minian_folder := sorted(self.basedir.glob("**/minian"))) == 1
+            ), "More than one minian folder found, fill in directory manually"
+            self.minian_folder = Path(minian_folder[0])
+
+        self.minian = {
+            "A": np.load(self.minian_folder / "A.npy"),
+            "C": np.load(self.minian_folder / "C.npy"),
+            "S": np.load(self.minian_folder / "S.npy"),
+        }
+
+    def load_all_timestamps(self, format="UCLA"):
+        """Loads multiple timestamps from multiple videos in the UCLA miniscope software file format
+        (folder = ""hh_mm_ss")
+
+        :param basedir: two levels up from all your
+        :param format: str, either 'UCLA' (default) to use the UCLA miniscope folder format or a regex if you are
+        using a different folder naming convention.
+        :return:
+        """
+
+        # Get recording folders, sorted by time of acquisition
+        if format == "UCLA":
+            search_regex = "**/[0-1][0-9]_[0-6][0-9]_[0-6][0-9]"
+        self.rec_folders = sorted(self.basedir.glob(search_regex))
+
+        # Loop through and load all timestamps, then concatenate
+        times_list = []
+        for rec_folder in self.rec_folders:
+            times_temp, _, _, _ = load_timestamps(
+                rec_folder, corrupted_videos="from_file"
+            )
+            times_list.append(times_temp)
+
+        self.times_all = pd.concat(times_list)
+
+        return self.times_all
 
 
 def load_timestamps(rec_folder, corrupted_videos=None):
     """Loads in timestamps corresponding to all videos in rec_folder.
 
     :param rec_folder: str or path
-    :param corrupted_videos: list of indices of corrupted files (e.g. [4, 6] would mean the 5th and 7th videos
-    were corrupted and should be skipped - their timestamps will be ommitted from the output.
+    :param corrupted_videos: (default) list of indices of corrupted files (e.g. [4, 6] would mean the 5th and 7th videos
+    were corrupted and should be skipped - their timestamps will be omitted from the output.
+    'from_file' will automatically grab any video indices provided as a csv file named 'corrupted_videos.csv'
     :return:
     """
 
     rec_folder = Path(rec_folder)
+
+    # Get video folder name from metaData.json file
+    with open(rec_folder / "metaData.json", "rb") as f:
+        rec_metadata = json.load(f)
     assert (
-        len(vid_folders := sorted(rec_folder.glob("*Miniscope"))) == 1
+        len(rec_metadata["miniscopes"]) == 1
+    ), "More/less than one miniscope detected in this recording"
+    ms_folder_name = rec_metadata["miniscopes"][0].replace(" ", "_")
+
+    # Auto detect folder where videos live
+    assert (
+        len(vid_folders := sorted(rec_folder.glob("*" + ms_folder_name))) == 1
     ), "Multiple Miniscope folders present, fix folders or code"
     vid_folder = vid_folders[0]
 
     # Get info about frame rate, gain, frames per file, etc.
     with open(str(vid_folder / "metaData.json"), "rb") as f:
         vid_metadata = json.load(f)
-
-    # Get more specific info (e.g. Rat Name, baseDirectory, and recording start time parameters
-    with open(str(rec_folder / "metaData.json"), "rb") as f:
-        rec_metadata = json.load(f)
 
     # Derive start_time from rec_metadata
     rec_start = rec_metadata["recordingStartTime"]
@@ -54,18 +95,44 @@ def load_timestamps(rec_folder, corrupted_videos=None):
     timestamps = start_time["start_time"] + pd.to_timedelta(
         times["Time Stamp (ms)"], unit="ms"
     )
-    times["Timestamps"] = times
+    times["Timestamps"] = timestamps
     nframes = times.shape[0]
 
     # Last, remove any rows for corrupted videos
-    good_frame_bool = np.ones(nframes, dtype=bool)
-    f_per_file = vid_metadata["framesPerFile"]
-    for corrupt_vid in corrupted_videos:
-        good_frame_bool[
-            range(
-                corrupt_vid * f_per_file,
-                np.min((f_per_file * (corrupt_vid + 1), nframes)),
-            )
-        ] = 0
+    if corrupted_videos == "from_file":
+        good_frame_bool = np.ones(nframes, dtype=bool)
+        f_per_file = vid_metadata["framesPerFile"]
+        assert (
+            len(corrupt_file := sorted(vid_folder.glob("corrupted_videos.csv"))) <= 1
+        ), ("More than one " "corrupted_videos.csv" " file found")
+        if len(corrupt_file) == 1:
+            corrupted_videos = pd.read_csv(corrupt_file[0], header=None)[0]
+        else:
+            corrupted_videos = []
 
-    return timestamps, rec_metadata, vid_metadata, rec_start
+        for corrupt_vid in corrupted_videos:
+            print(
+                "Eliminating timestamps from corrupted video"
+                + str(corrupt_vid)
+                + " in "
+                + str(rec_folder.parts[-1] + " folder.")
+            )
+            good_frame_bool[
+                range(
+                    corrupt_vid * f_per_file,
+                    np.min((f_per_file * (corrupt_vid + 1), nframes)),
+                )
+            ] = 0
+
+        times = times[good_frame_bool]
+
+    return times, rec_metadata, vid_metadata, rec_start
+
+
+if __name__ == "__main__":
+    basedir = Path("/data/Working/Trace_FC/Recording_Rats/Rat698/2021_06_30_recall")
+    basedir2 = Path(
+        "/data/Working/Trace_FC/Recording_Rats/Rat698/2021_06_30_recall/2_tone_recall"
+    )
+    msobj = MiniscopeIO()
+    msobj.load_all_timestamps(basedir)
