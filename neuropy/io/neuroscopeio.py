3d15a8d722fe2d2c15ddee19fe4605a0ea127c2b
bapungiri <bapun.k.giri@gmail.com>
2021-10-14 14:42:38 -0400

    bugfix

--- neuropy/io/neuroscopeio.py
+++ neuropy/io/neuroscopeio.py
@@ -73,7 +73,7 @@ class NeuroscopeIO:
             list of spike times.
         """
 
-        spks = neurons.get_all_spikes()
+        spks = neurons.spiketrains
         srate = neurons.sampling_rate
         nclu = len(spks)
         spk_frame = np.concatenate([(cell * srate).astype(int) for cell in spks])
