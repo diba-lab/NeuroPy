616626b14f7e81669006830ada2d3d7da1120515
Pho Hale <PhoHale@gmail.com>
2021-11-28 05:34:24 -0500

    added laps

---
+++ neuropy/core/laps.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+from .datawriter import DataWriter
+
+from ..utils.mixins.time_slicing import StartStopTimesMixin, TimeSlicableObjectProtocol, TimeSlicableIndiciesMixin
+from ..utils.mixins.unit_slicing import NeuronUnitSlicableObjectProtocol
+
+
+# TODO: implement: NeuronUnitSlicableObjectProtocol, StartStopTimesMixin, TimeSlicableObjectProtocol
+class Laps(DataWriter):
+    """Class to hold computed info about laps and how they relate to other information like times, flat linear indicies, etc.
+    
+    ## TODO: Look at Epoch class for implementation guidance 
+    """
+
+    def __init__(
+        self,
+        lap_id: np.ndarray,
+        laps_spike_counts=None,
+        lap_start_stop_flat_idx=None,
+        lap_start_stop_time=None,
+        metadata=None,
+    ) -> None:
+        super().__init__(metadata=metadata)
+
+        self.lap_id = np.array(lap_id)
+        assert (len(laps_spike_counts) == len(lap_id)), "laps_spike_counts first dimension must match number of laps"
+        self.laps_spike_counts = laps_spike_counts
+        assert (lap_start_stop_flat_idx.shape[0] == len(lap_id)), "lap_start_stop_flat_idx first dimension must match number of laps"
+        self.lap_start_stop_flat_idx = lap_start_stop_flat_idx
+        assert (lap_start_stop_time.shape[0] == len(lap_id)), "lap_start_stop_time first dimension must match number of laps"
+        self.lap_start_stop_time = lap_start_stop_time
+
+    @property
+    def n_laps(self):
+        return len(self.lap_id)
+    
+	@staticmethod
+    def from_file(f):
+        d = DataWriter.from_file(f)
+        if d is not None:
+            return Laps.from_dict(d)
+        else:
+            return None
