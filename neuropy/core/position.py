c435fdd76f0ffeecb325c6defe108079f406379c
Pho Hale <PhoHale@gmail.com>
2022-01-07 04:30:40 -0500

    cleaned up all the old functions that were refactored into the MIXINS

--- neuropy/core/position.py
+++ neuropy/core/position.py
@@ -17,6 +17,7 @@ from neuropy.utils.mixins.time_slicing import StartStopTimesMixin, TimeSlicableO
 from neuropy.utils.mixins.concatenatable import ConcatenationInitializable
 from neuropy.utils.mixins.dataframe_representable import DataFrameRepresentable
 
+""" --- Helper MIXINS """
 class PositionDimDataMixin:
     """ Implementors gain convenience properties to access .x, .y, and .z variables as properties. 
     Requirements:
@@ -121,10 +122,8 @@ class PositionDimDataMixin:
     def n_frames(self):
         return len(self.df.index)
 
-
 class PositionComputedDataMixin:
-    
-    # PositionDimDataMixin
+    """ Requires conformance to PositionDimDataMixin as well."""
     
     ## Computed Variable Labels Properties:
     @staticmethod
@@ -315,13 +314,10 @@ class PositionComputedDataMixin:
         return self.df['acceleration_y'].to_numpy()
     
     
-    
-    
 """ --- """
 @pd.api.extensions.register_dataframe_accessor("position")
 class PositionAccessor(PositionDimDataMixin, PositionComputedDataMixin, TimeSlicedMixin):
     """ A Pandas DataFrame-based Position helper. """
-    # __time_variable_name = 't' # currently hardcoded
     
     def __init__(self, pandas_obj):
         self._validate(pandas_obj)
@@ -337,63 +333,13 @@ class PositionAccessor(PositionDimDataMixin, PositionComputedDataMixin, TimeSlic
         # if "lin_pos" not in obj.columns or "speed" not in obj.columns:
         #     raise AttributeError("Must have 'lin_pos' column and 'x'.")
 
-    # for PositionDimDataMixin
+    # for PositionDimDataMixin & PositionComputedDataMixin
     @property
     def df(self):
         return self._obj # for PositionAccessor
     @df.setter
     def df(self, value):
         self._obj = value # for PositionAccessor
-    
-    # @property
-    # def time_variable_name(self):
-    #     return PositionAccessor.__time_variable_name
-    
-    # @property
-    # def ndim(self):
-    #     """ returns the count of the spatial columns that the dataframe has """
-    #     return np.sum(np.isin(['x','y','z'], self._obj.columns))
-        
-    # @property
-    # def dim_columns(self):
-    #     """ returns the labels of the columns that correspond to spatial columns 
-    #         If ndim == 1, returns ['x'], 
-    #         if ndim == 2, returns ['x','y'], etc.
-    #     """
-    #     spatial_column_labels = np.array(['x','y','z'])
-    #     return list(spatial_column_labels[np.isin(spatial_column_labels, self._obj.columns)])
-    
-    # @property
-    # def n_frames(self):
-    #     return len(self._obj.index)
-    
-    # @property
-    # def time(self):
-    #     return self._obj[self.time_variable_name].to_numpy()
-    
-    # @property
-    # def traces(self):
-    #     """ Compatibility method for the old-style implementation. """
-    #     # print('traces accessed with self.ndim of {}'.format(self.ndim))
-    #     if self.ndim == 1:
-    #         return self._obj[['x']].to_numpy().T
-    #     elif self.ndim >= 2:
-    #         return self._obj[['x','y']].to_numpy().T
-    #     elif self.ndim >= 3:
-    #         return self._obj[['x','y','z']].to_numpy().T
-    #     else:
-    #         raise IndexingError
-        
-    # @property
-    # def speed(self):
-    #     # dt = 1 / self.sampling_rate
-    #     if 'speed' in self._obj.columns:
-    #         return self._obj['speed'].to_numpy()
-    #     else:
-    #         # dt = np.diff(self.time)
-    #         dt = np.mean(np.diff(self.time))
-    #         self._obj['speed'] = np.insert((np.sqrt(((np.abs(np.diff(self.traces, axis=1))) ** 2).sum(axis=0)) / dt), 0, 0.0) # prepends a 0.0 value to the front of the result array so it's the same length as the other position vectors (x, y, etc)        
-    #     return self._obj['speed'].to_numpy()
     
     
     # # TODO: The documentation says this is the preferred method, but it doesn't actually seem to be used anywhere!
@@ -422,7 +368,6 @@ class PositionAccessor(PositionDimDataMixin, PositionComputedDataMixin, TimeSlic
     #     self._obj[acceleration_column_key] = acceleration_comp
         
     #     return self._obj  
-    
     
     
 """ --- """
@@ -498,116 +443,6 @@ class Position(PositionDimDataMixin, PositionComputedDataMixin, ConcatenationIni
     def df(self, value):
         self._data = value # for Position
 
-    # @property
-    # def traces(self):
-    #     """ Compatibility method for the old-style implementation. """
-    #     # print('traces accessed with self.ndim of {}'.format(self.ndim))
-    #     if self.ndim == 1:
-    #         return self._data[['x']].to_numpy().T
-    #     elif self.ndim >= 2:
-    #         return self._data[['x','y']].to_numpy().T
-    #     elif self.ndim >= 3:
-    #         return self._data[['x','y','z']].to_numpy().T
-    #     else:
-    #         raise IndexingError
-    
-
-    # @property
-    # def x(self):
-    #     return self._data['x'].to_numpy()
-
-    # @x.setter
-    # def x(self, x):
-    #     self._data.loc[:, 'x'] = x
-
-    # @property
-    # def y(self):
-    #     assert self.ndim > 1, "No y for one-dimensional position"
-    #     return self._data['y'].to_numpy()
-
-    # @y.setter
-    # def y(self, y):
-    #     assert self.ndim > 1, "Position data has only one dimension"
-    #     self._data.loc[:, 'y'] = y
-
-    # @property
-    # def z(self):
-    #     assert self.ndim == 3, "Position data is not three-dimensional"
-    #     return self._data['z'].to_numpy()
-
-    # @z.setter
-    # def z(self, z):
-    #     self._data.loc[:, 'z'] = z
-
-    # @property
-    # def linear_pos_obj(self):
-    #     # returns a Position object containing only the linear_pos as its trace. This is used for compatibility with Bapun's Pf1D function 
-    #     lin_pos_df = self._data[[self.time_variable_name,'lin_pos']].copy()
-    #     # lin_pos_df.rename({'lin_pos':'x'}, axis='columns', errors='raise', inplace=True)
-    #     lin_pos_df['x'] = lin_pos_df['lin_pos'].copy() # duplicate the lin_pos column to the 'x' column
-    #     out_obj = Position(lin_pos_df, metadata=self.metadata)
-    #     out_obj.speed; # ensure speed is calculated
-    #     return out_obj
-
-    # @property
-    # def linear_pos(self):
-    #     assert 'lin_pos' in self._data.columns, "Linear Position data has not yet been computed."
-    #     return self._data['lin_pos'].to_numpy()
-
-    # @linear_pos.setter
-    # def linear_pos(self, linear_pos):
-    #     self._data.loc[:, 'lin_pos'] = linear_pos
-        
-        
-    # @property
-    # def has_linear_pos(self):
-    #     if 'lin_pos' in self._data.columns:
-    #         return not np.isnan(self._data['lin_pos'].to_numpy()).all() # check if all are nan
-    #     else:
-    #         # Linear Position data has not yet been computed.
-    #         return False
-
-    # @property
-    # def t_start(self):
-    #     return self._data['t'].iloc[0]
-    # @t_start.setter
-    # def t_start(self, t):
-    #     raise NotImplementedError
-    #     # self._t_start = t
-    # @property
-    # def t_stop(self):
-    #     return self._data['t'].iloc[-1]
-    # @property
-    # def duration(self):
-    #     return self.t_stop - self.t_start
-    #     # return float(self.n_frames) / float(self.sampling_rate)
-
-
-    
-    # @property
-    # def n_frames(self):
-    #     return len(self._data.index)
-    #     # return self.traces.shape[1]
-
-    
-    # @property
-    # def time(self):
-    #     return self._data['t'].to_numpy()
-
-    # @property
-    # def ndim(self):
-    #     # returns the count of the spatial columns that the dataframe has
-    #     return np.sum(np.isin(['x','y','z'], self._data.columns))
-    #     # return self.traces.shape[0]
-        
-    # @property
-    # def dim_columns(self):
-    #     # returns the labels of the columns that correspond to spatial columns 
-    #     # If ndim == 1, returns ['x'], 
-    #     # if ndim == 2, returns ['x','y'], etc.
-    #     spatial_column_labels = np.array(['x','y','z'])
-    #     return list(spatial_column_labels[np.isin(spatial_column_labels, self._data.columns)])
-
     @property
     def sampling_rate(self):
         # raise NotImplementedError
@@ -631,167 +466,7 @@ class Position(PositionDimDataMixin, PositionComputedDataMixin, ConcatenationIni
             d["df"],
             metadata=d["metadata"],
         )
-            
-    # @property
-    # def speed(self):
-    #     # dt = 1 / self.sampling_rate
-    #     if 'speed' in self._data.columns:
-    #         return self._data['speed'].to_numpy()
-    #     else:
-    #         # dt = np.diff(self.time)
-    #         dt = np.mean(np.diff(self.time))
-    #         self._data['speed'] = np.insert((np.sqrt(((np.abs(np.diff(self.traces, axis=1))) ** 2).sum(axis=0)) / dt), 0, 0.0) # prepends a 0.0 value to the front of the result array so it's the same length as the other position vectors (x, y, etc)        
-    #     return self._data['speed'].to_numpy()
-    
-    
-    # TODO: implement velocity/acceleration properties that call the self.compute_higher_order_derivatives() if needed, otherwise return the appopriate column 
-    
-    # @property
-    # def velocity(self):
-    #     # dt = 1 / self.sampling_rate
-    #     if 'speed' in self._data.columns:
-    #         return self._data['speed'].to_numpy()
-    #     else:
-    #         # dt = np.diff(self.time)
-    #         dt = np.mean(np.diff(self.time))
-    #         self._data['speed'] = np.insert((np.sqrt(((np.abs(np.diff(self.traces, axis=1))) ** 2).sum(axis=0)) / dt), 0, 0.0) # prepends a 0.0 value to the front of the result array so it's the same length as the other position vectors (x, y, etc)        
-    #     return self._data['speed'].to_numpy()
-    
-    # @property
-    # def dim_columns(self):
-    #     # returns the labels of the columns that correspond to spatial columns 
-    #     # If ndim == 1, returns ['x'], 
-    #     # if ndim == 2, returns ['x','y'], etc.
-    #     spatial_column_labels = np.array(['x','y','z'])
-    #     return list(spatial_column_labels[np.isin(spatial_column_labels, self._data.columns)])
-    
-    
-    
-    # @property
-    # def dt(self):
-    #     if 'dt' in self._data.columns:
-    #         return self._data['dt'].to_numpy()
-    #     else:
-    #         # compute the higher_order_derivatives if not already done upon first access
-    #         self._data = self.compute_higher_order_derivatives()   
-    #     return self._data['dt'].to_numpy()
-    
-    # @property
-    # def velocity_x(self):
-    #     if 'velocity_x' in self._data.columns:
-    #         return self._data['velocity_x'].to_numpy()
-    #     else:
-    #         # compute the higher_order_derivatives if not already done upon first access
-    #         self._data = self.compute_higher_order_derivatives()   
-    #     return self._data['velocity_x'].to_numpy()
-    
-    # @property
-    # def acceleration_x(self):
-    #     if 'acceleration_x' in self._data.columns:
-    #         return self._data['acceleration_x'].to_numpy()
-    #     else:
-    #         # compute the higher_order_derivatives if not already done upon first access
-    #         self._data = self.compute_higher_order_derivatives()   
-    #     return self._data['acceleration_x'].to_numpy()
-    
-    # @property
-    # def velocity_y(self):
-    #     if 'velocity_y' in self._data.columns:
-    #         return self._data['velocity_y'].to_numpy()
-    #     else:
-    #         # compute the higher_order_derivatives if not already done upon first access
-    #         self._data = self.compute_higher_order_derivatives()   
-    #     return self._data['velocity_y'].to_numpy()
-    
-    # @property
-    # def acceleration_y(self):
-    #     if 'acceleration_y' in self._data.columns:
-    #         return self._data['acceleration_y'].to_numpy()
-    #     else:
-    #         # compute the higher_order_derivatives if not already done upon first access
-    #         self._data = self.compute_higher_order_derivatives()   
-    #     return self._data['acceleration_y'].to_numpy()
-    
-    
-    
-    
-    # @staticmethod
-    # def _computed_column_component_labels(component_label):
-    #     return [f'velocity_{component_label}', f'acceleration_{component_label}']
-    
-    # def compute_higher_order_derivatives(self):
-    #     """Computes the higher-order positional derivatives for all spatial dimensional components of self._data. Adds the dt, velocity, and acceleration columns
-    #     """
-    #     for dim_i in np.arange(self.ndim):
-    #         curr_column_label = self.dim_columns[dim_i]
-    #         self._data = Position._perform_compute_higher_order_derivatives(self._data, curr_column_label)
-    #         # self._data = self._data.position._perform_compute_higher_order_derivatives(curr_column_label) # uses the position accessor
-    #     return self._data
-                   
-    
-    # ## TODO: _perform_compute_higher_order_derivatives has been depricated in favor of factoring this functionality into the pho_custom_placefields "position" pandas Dataframe extension. (See PositionAccessor)
-    # @staticmethod
-    # def _perform_compute_higher_order_derivatives(pos_df: pd.DataFrame, component_label: str):
-    #     """Computes the higher-order positional derivatives for a single component (given by component_label) of the pos_df
-    #     Args:
-    #         pos_df (pd.DataFrame): [description]
-    #         component_label (str): [description]
-    #     Returns:
-    #         pd.DataFrame: The updated dataframe with the dt, velocity, and acceleration columns added.
-    #     """
-    #     # compute each component separately:
-    #     velocity_column_key = f'velocity_{component_label}'
-    #     acceleration_column_key = f'acceleration_{component_label}'
-                
-    #     dt = np.insert(np.diff(pos_df['t']), 0, np.nan)
-    #     velocity_comp = np.insert(np.diff(pos_df[component_label]), 0, 0.0) / dt
-    #     velocity_comp[np.isnan(velocity_comp)] = 0.0 # replace NaN components with zero
-    #     acceleration_comp = np.insert(np.diff(velocity_comp), 0, 0.0) / dt
-    #     acceleration_comp[np.isnan(acceleration_comp)] = 0.0 # replace NaN components with zero
-    #     dt[np.isnan(dt)] = 0.0 # replace NaN components with zero
-        
-    #     # add the columns to the dataframe:
-    #     pos_df['dt'] = dt
-    #     pos_df[velocity_column_key] = velocity_comp
-    #     pos_df[acceleration_column_key] = acceleration_comp
-        
-    #     return pos_df  
-    
-    
-    # def compute_smoothed_position_info(self, N: int = 20, non_smoothed_column_labels=None):
-    #     """Computes smoothed position variables and adds them as columns to the internal dataframe
-    #     Args:
-    #         N (int, optional): [description]. Defaults to 20.
-    #     Returns:
-    #         [type]: [description]
-    #     """
-    #     if non_smoothed_column_labels is None:
-    #         non_smoothed_column_labels = (self.dim_columns + self.dim_computed_columns)
-    #     self._data = Position._perform_compute_smoothed_position_info(self._data, non_smoothed_column_labels, N=N)
-    #     return self._data
-    
-        
-    # @staticmethod
-    # def _perform_compute_smoothed_position_info(pos_df: pd.DataFrame, non_smoothed_column_labels, N: int = 20):
-    #     """Computes the higher-order positional derivatives for a single component (given by component_label) of the pos_df
-    #     Args:
-    #         pos_df (pd.DataFrame): [description]
-    #         non_smoothed_column_labels (list(str)): a list of the columns to be smoothed
-    #         N (int): 20 # roll over the last N samples
-            
-    #     Returns:
-    #         pd.DataFrame: The updated dataframe with the dt, velocity, and acceleration columns added.
-            
-    #     Usage:
-    #         smoothed_pos_df = Position._perform_compute_smoothed_position_info(pos_df, (position_obj.dim_columns + position_obj.dim_computed_columns), N=20)
-        
-    #     """
-    #     smoothed_column_names = [f'{a_label}_smooth' for a_label in non_smoothed_column_labels]
-    #     # non_smoothed_column_labels = position_obj.dim_columns + position_obj.dim_computed_columns
-    #     # smoothed_pos_df = pos_df[non_smoothed_column_labels].rolling(window=N).mean()
-    #     pos_df[smoothed_column_names] = pos_df[non_smoothed_column_labels].rolling(window=N).mean()
-    #     return pos_df
-        
+
     # @staticmethod
     # def is_fixed_sampling_rate(time):
     #     dt = np.diff(time)
