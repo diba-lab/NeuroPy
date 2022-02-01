09e4278df390db92aa146d49af9c8486c1f6dc51
bapungiri <bapun.k.giri@gmail.com>
2022-02-01 16:47:32 -0500

    new file for hosting data visualizations plots

---
+++ neuropy/plotting/dataviz.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+
+"""Hosts some frequently plotted data visualizations
+"""
+
+
+def lineplot(data: pd.DataFrame, x, y, hue, palette, ci="sem", ax=None):
+    """Seaborn type lineplot but with sem errorbars
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        long form pandas dataframe
+    x : str
+        column specifying the x variable
+    y : str
+        column specifying the y variable
+    hue : str
+        grouping variable
+    palette : colormap
+        colormap for each nested group
+    ci : str, optional
+        error bars, by default "sem"
+    ax : [type], optional
+        ax to plot on, by default None
+    """
+    hues = np.unique(data[hue].values)
+    for i, h in enumerate(hues):
+        df = data[data[hue] == h]
+        group = df.groupby(x)
+        mean_y = group.mean()[y]
+        x_val = mean_y.index
+        err = group.sem(ddof=0)[y]
+        ax.fill_between(
+            x_val, mean_y - err, mean_y + err, color=palette[i], alpha=0.4, ec="none"
+        )
+        ax.plot(x_val, mean_y, color=palette[i])
+
+    return ax
