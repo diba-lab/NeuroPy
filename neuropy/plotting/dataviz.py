d8a420f0593c6c1c05dc9c42beccf02552151e34
bapungiri <bapun.k.giri@gmail.com>
2022-03-24 13:42:01 -0400

    new and enh for dataviz plots

--- neuropy/plotting/dataviz.py
+++ neuropy/plotting/dataviz.py
@@ -7,7 +7,7 @@ import seaborn as sns
 """
 
 
-def lineplot(data: pd.DataFrame, x, y, hue, palette, ci="sem", ax=None):
+def lineplot(data: pd.DataFrame, x, y, hue=None, palette=None, ci="sem", ax=None):
     """Seaborn type lineplot but with sem errorbars
 
     Parameters
@@ -23,17 +23,42 @@ def lineplot(data: pd.DataFrame, x, y, hue, palette, ci="sem", ax=None):
     palette : colormap
         colormap for each nested group
     ci : str, optional
-        standard error of mean (sem), mean abs deviation (mad), standard deviation (sd), by default "sem"
+        standard error of mean (sem), mean abs deviation (mad), standard deviation (sd),
+        dataframe column name for custom error bars, by default "sem"
     ax : [type], optional
         ax to plot on, by default None
     """
-    hues = np.unique(data[hue].values)
-    for i, h in enumerate(hues):
-        df = data[data[hue] == h]
+
+    if hue is not None:
+        hues = np.unique(data[hue].values)
+        for i, h in enumerate(hues):
+            df = data[data[hue] == h]
+            group = df.groupby(x)
+            mean_y = group.mean()[y]
+            x_val = mean_y.index
+            err = group.sem(ddof=0)[y]
+            ax.fill_between(
+                x_val,
+                mean_y - err,
+                mean_y + err,
+                color=palette[i],
+                alpha=0.4,
+                ec="none",
+            )
+            ax.plot(x_val, mean_y, color=palette[i])
+
+    else:
+        assert len(palette) == 1, "Wrong number of colors in palette"
+        df = data
         group = df.groupby(x)
         mean_y = group.mean()[y]
         x_val = mean_y.index
-        err = group.sem(ddof=0)[y]
+
+        if ci in df.columns():
+            err = df[ci]
+        elif ci == "sem":
+            err = group.sem(ddof=0)[y]
+
         ax.fill_between(
             x_val, mean_y - err, mean_y + err, color=palette[i], alpha=0.4, ec="none"
         )
@@ -185,6 +210,87 @@ def pointplot(
         for i, x_val in enumerate(x_cat):
             box_data = data[data[x] == x_val][y]
             ax.boxplot(box_data, positions=[i], widths=width, **props(palette[i]))
+            ax.set_xticks(np.arange(len(x_cat)), x_cat)
+
+    return ax
+
+
+def barplot(
+    data: pd.DataFrame,
+    x,
+    y,
+    hue=None,
+    hue_palette=None,
+    x_palette=None,
+    ax=None,
+    width=0.8,
+):
+    """Seaborn type boxplot but all lines associated with a corresponding boxplot will have same colors.
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        long form pandas dataframe
+    x : str
+        column specifying the x variable
+    y : str
+        column specifying the y variable
+    hue : str
+        grouping variable
+    palette : colormap
+        colormap for each nested group
+    ci : str, optional
+        standard error of mean (sem), mean abs deviation (mad), standard deviation (sd), by default "sem"
+    ax : [type], optional
+        ax to plot on, by default None
+    """
+    props = lambda c: dict(
+        edgecolor=c,
+        color="w",
+        ecolor=c,
+        linewidth=1.2,
+        error_kw=dict(elinewidth=0.8),
+        capsize=1.5,
+    )
+
+    if ax is None:
+        _, ax = plt.subplots()
+    x_cat = data[x].unique()
+
+    if hue is not None:
+        hue_levels = np.unique(data[hue])
+        n_levels = len(hue_levels)
+        each_width = width / n_levels
+        offsets = np.linspace(0, width - each_width, n_levels)
+        offsets -= offsets.mean()
+
+        if (hue_palette is None) and (x_palette is not None):
+            colors = np.concatenate(
+                [[x_palette[i]] * n_levels for i in range(len(x_cat))]
+            )
+
+        i2 = 0
+        for i, x_val in enumerate(x_cat):
+            for i1, hue_level in enumerate(hue_levels):
+                center = i + offsets[i1]
+                bar_data = data[(data[x] == x_val) & (data[hue] == hue_level)][y]
+                bar_mean = bar_data.mean()
+                bar_err = bar_data.sem(ddof=0)
+                ax.bar(
+                    x=center,
+                    height=bar_mean,
+                    width=each_width - 0.05,
+                    **props(colors[i2]),
+                    yerr=bar_err,
+                )
+                i2 += 1
+
+        ax.set_xticks(np.arange(len(x_cat)), x_cat)
+    else:
+
+        for i, x_val in enumerate(x_cat):
+            box_data = data[data[x] == x_val][y]
+            ax.boxplot(box_data, positions=[i], widths=width, **props(x_palette[i]))
             ax.set_xticks(np.arange(len(x_cat)), x_cat)
 
     return ax
