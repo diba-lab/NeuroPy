01f06918c18f1f77d86967eab147cf315a6f4a88
Pho Hale <CommanderPho@users.noreply.github.com>
2022-05-31 15:48:49 -0400

    updated requirements

--- PhoLoadBapun.py
+++ PhoLoadBapun.py
@@ -3,8 +3,6 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import scipy.stats as stats
-import seaborn as sns
 
 ## Try adding NeuroPy repo path to the PythonPath
 import sys
@@ -19,21 +17,19 @@ except ImportError:
 	from neuropy import core
 
 
-# from callfunc import processData
-from ProcessData import ProcessData
+from neuropy.core.session.data_session_loader import DataSessionLoader
 
-#%% Subjects
-basePath = [
-	"/Volumes/iNeo/Data/Bapun/Day5TwoNovel/"
-]
-# basePath = [
-#     "/data/Clustering/SleepDeprivation/RatN/Day2/",
-#     "/data/Clustering/SleepDeprivation/RatK/Day4/"
-#     "/data/Clustering/SleepDeprivation/RatN/Day4/"
-# ]
+## Bapun Format:
+# basedir = '/media/share/data/Bapun/Day5TwoNovel' # Linux
+basedir = Path('R:\data\Bapun\Day5TwoNovel') # Windows
+# basedir = '/Volumes/iNeo/Data/Bapun/Day5TwoNovel' # MacOS
 
+sess = DataSessionLoader.bapun_data_session(basedir)
+active_sess_config = sess.config
+session_name = sess.name
+print('session dataframe spikes: {}\n'.format(sess.spikes_df.shape))
 
+print('session dataframe spikes: {}\nsession.neurons.n_spikes summed: {}\n'.format(sess.spikes_df.shape, np.sum(sess.neurons.n_spikes)))
 
-sessions = [ProcessData(_) for _ in basePath
 # sess = sessions[0]
 
