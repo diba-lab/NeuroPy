538b6537e6d6d4387e64288886b4903ae8786100
Pho Hale <CommanderPho@users.noreply.github.com>
2022-07-04 22:06:52 -0400

    Changed KnownDataSessionTypeProperties to include an explicit __init__ function for future extendability. Was previously a dataclass

--- neuropy/core/session/KnownDataSessionTypeProperties.py
+++ neuropy/core/session/KnownDataSessionTypeProperties.py
@@ -1,11 +1,11 @@
 import dataclasses
-from dataclasses import dataclass
+# from dataclasses import dataclass
 from pathlib import Path
 from typing import Callable, List
+from neuropy.utils.mixins.print_helpers import SimplePrintable
 
-
-@dataclass
-class KnownDataSessionTypeProperties(object):
+# @dataclass
+class KnownDataSessionTypeProperties(SimplePrintable, object):
     """Docstring for KnownDataSessionTypeProperties."""
     load_function: Callable
     basedir: Path
@@ -14,6 +14,13 @@ class KnownDataSessionTypeProperties(object):
     filter_functions: List[Callable] = dataclasses.field(default_factory=list)
     post_compute_functions: List[Callable] = dataclasses.field(default_factory=list)
     
-    
-    def update_basedir(self, updated_basedir):
-        self.basedir = updated_basedir
+    def __init__(self, load_function: Callable, basedir: Path, post_load_functions: List[Callable] = dataclasses.field(default_factory=list), filter_functions: List[Callable] = dataclasses.field(default_factory=list), post_compute_functions: List[Callable] = dataclasses.field(default_factory=list)) -> None:
+        self.load_function = load_function
+        self.basedir = basedir
+        # Optional properties:
+        self.post_load_functions = post_load_functions
+        self.filter_functions = filter_functions
+        self.post_compute_functions = post_compute_functions
+
+    # def update_basedir(self, updated_basedir):
+    #     self.basedir = updated_basedir
