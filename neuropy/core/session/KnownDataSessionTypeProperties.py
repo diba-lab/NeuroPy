3835efc080618e91a3edf9335fecc957dcaa9e83
Pho Hale <CommanderPho@users.noreply.github.com>
2022-07-04 22:09:50 -0400

    cleanup the dataclass stuff

--- neuropy/core/session/KnownDataSessionTypeProperties.py
+++ neuropy/core/session/KnownDataSessionTypeProperties.py
@@ -1,26 +1,37 @@
 import dataclasses
-# from dataclasses import dataclass
 from pathlib import Path
 from typing import Callable, List
 from neuropy.utils.mixins.print_helpers import SimplePrintable
 
-# @dataclass
+
 class KnownDataSessionTypeProperties(SimplePrintable, object):
     """Docstring for KnownDataSessionTypeProperties."""
     load_function: Callable
     basedir: Path
     # Optional members
-    post_load_functions: List[Callable] = dataclasses.field(default_factory=list)
-    filter_functions: List[Callable] = dataclasses.field(default_factory=list)
-    post_compute_functions: List[Callable] = dataclasses.field(default_factory=list)
+    post_load_functions: List[Callable]
+    filter_functions: List[Callable]
+    post_compute_functions: List[Callable]
     
-    def __init__(self, load_function: Callable, basedir: Path, post_load_functions: List[Callable] = dataclasses.field(default_factory=list), filter_functions: List[Callable] = dataclasses.field(default_factory=list), post_compute_functions: List[Callable] = dataclasses.field(default_factory=list)) -> None:
+    def __init__(self, load_function: Callable, basedir: Path, post_load_functions: List[Callable] = None, filter_functions: List[Callable] = None, post_compute_functions: List[Callable] = None) -> None:
         self.load_function = load_function
         self.basedir = basedir
         # Optional properties:
-        self.post_load_functions = post_load_functions
-        self.filter_functions = filter_functions
-        self.post_compute_functions = post_compute_functions
+        if post_load_functions is not None:
+            self.post_load_functions = post_load_functions
+        else:
+            self.post_load_functions = []
+
+        if filter_functions is not None:
+            self.filter_functions = filter_functions
+        else:
+            self.filter_functions = []
+
+        if post_compute_functions is not None:
+            self.post_compute_functions = post_compute_functions
+        else:
+            self.post_compute_functions = []
+
 
     # def update_basedir(self, updated_basedir):
     #     self.basedir = updated_basedir
