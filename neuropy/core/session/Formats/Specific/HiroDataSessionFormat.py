34610277d009843aca15d927931a048e8eacc99b
Pho Hale <CommanderPho@users.noreply.github.com>
2022-07-08 06:43:20 -0400

    added code to load additional exported fields

--- neuropy/core/session/Formats/Specific/HiroDataSessionFormat.py
+++ neuropy/core/session/Formats/Specific/HiroDataSessionFormat.py
@@ -269,8 +269,8 @@ class HiroDataSessionFormatRegisteredClass(DataSessionFormatBaseRegisteredClass)
         pos_vars = DynamicContainer(t=t,x=x,y=y,speeds=speeds,dt=dt,dx=dx,dy=dy)
         
         # Import the spikes
-        spike_matrix, spike_cells, num_cells, spike_list, spike_positions_list, flat_cell_ids, reverse_cellID_idx_lookup_map = cls.perform_import_spikes(t, x, y, mat_import_parent_path=mat_import_parent_path)
-        spikes_vars = DynamicContainer(spike_matrix=spike_matrix, spike_cells=spike_cells, num_cells=num_cells, spike_list=spike_list, spike_positions_list=spike_positions_list, flat_cell_ids=flat_cell_ids, reverse_cellID_idx_lookup_map=reverse_cellID_idx_lookup_map)
+        spike_matrix, spike_cells, num_cells, spike_list, spike_positions_list, flat_cell_ids, reverse_cellID_idx_lookup_map, spikes_cell_info_out_dict = cls.perform_import_spikes(t, x, y, mat_import_parent_path=mat_import_parent_path)
+        spikes_vars = DynamicContainer(spike_matrix=spike_matrix, spike_cells=spike_cells, num_cells=num_cells, spike_list=spike_list, spike_positions_list=spike_positions_list, flat_cell_ids=flat_cell_ids, reverse_cellID_idx_lookup_map=reverse_cellID_idx_lookup_map, spikes_cell_info_out_dict=DynamicContainer(**spikes_cell_info_out_dict))
         
         behavioral_periods, behavioral_epochs = cls.perform_import_extras(mat_import_parent_path=mat_import_parent_path)
         # behavioral_periods = all_results_data['active_processing/behavioral_periods_table']
@@ -314,7 +314,7 @@ class HiroDataSessionFormatRegisteredClass(DataSessionFormatBaseRegisteredClass)
         return dict(zip(flat_cell_ids, linear_flitered_ids))
 
     @classmethod
-    def perform_import_spikes(cls, t, x, y, mat_import_parent_path=Path(r'C:\Share\data\RoyMaze1')):
+    def perform_import_spikes(cls, t, x, y, mat_import_parent_path=Path(r'C:\Share\data\RoyMaze1'), debug_print=False):
         # Import the spikes
         # spikes_mat_import_file = mat_import_parent_path.joinpath('spikesTable.mat')
         spikes_mat_import_file = mat_import_parent_path.joinpath('ExportedData', 'spikesAnalysis.mat')
@@ -329,6 +329,37 @@ class HiroDataSessionFormatRegisteredClass(DataSessionFormatBaseRegisteredClass)
         except KeyError as e:
             print(f'KeyError: {e}')
             print(f'\t Valid Keys: {list(spikes_data.keys())}')
+            
+        ## Pho 2022-07-08 Extra fields exported from `C:\Users\pho\repos\PhoDibaLab_REM_HiddenMarkovModel\DEVELOPMENT\NeuroPyExporting2022\PhoNeuroPyConvert_ExportSpikesToPython.m`    
+        try:
+            mat_variables_to_extract = ['shank', 'cluster', 'aclu', 'qclu','speculated_unit_contamination_level','speculated_unit_type']
+            num_mat_variables = len(mat_variables_to_extract)
+            spikes_cell_info_out_dict = dict()
+            for i in np.arange(num_mat_variables):
+                curr_var_name = mat_variables_to_extract[i]
+                if curr_var_name == 'speculated_unit_type':
+                    ## This is a cellstr (cell array of char-strings):
+                    spikes_cell_info_out_dict[curr_var_name] = [str(an_item.item()[0]) for an_item in spikes_data[curr_var_name]] # ['pre_sleep', 'track', 'post_sleep']
+                else:
+                    # flat_spikes_out_dict[curr_var_name] = spikes_data[curr_var_name][0,0].flatten() # TODO: do we want .squeeze() instead of .flatten()??
+                    spikes_cell_info_out_dict[curr_var_name] = spikes_data[curr_var_name].flatten() # TODO: do we want .squeeze() instead of .flatten()?? NO! .flatten() works really well.
+                                        
+            if debug_print:
+                print(f'Successfully loaded the extra vars!')
+                print(spikes_cell_info_out_dict)
+            
+            
+            # spike_matrix = spikes_data['spike_matrix']
+            # spike_cells = spikes_data['spike_cells'][0]
+            # cell_ids = spikes_data['spike_cells_ids'][:,0].T
+            # flat_cell_ids = [int(cell_id) for cell_id in cell_ids]
+            
+        except KeyError as e:
+            print(f'KeyError: {e}.')
+            print(r'\t Did you export from Pho 2022-07-08 Extra fields exported from `C:\Users\pho\repos\PhoDibaLab_REM_HiddenMarkovModel\DEVELOPMENT\NeuroPyExporting2022\PhoNeuroPyConvert_ExportSpikesToPython.m`')
+            print(f'\t Valid Keys: {list(spikes_data.keys())}')
+            spikes_cell_info_out_dict = None
+            
 
         # print('spike_matrix: {}, spike_cells: {}'.format(np.shape(spike_matrix), np.shape(spike_cells)))
         num_cells = np.shape(spike_matrix)[0]
@@ -344,7 +375,7 @@ class HiroDataSessionFormatRegisteredClass(DataSessionFormatBaseRegisteredClass)
         # reverse_cellID_idx_lookup_map: Allows reverse indexing into the linear imported array using the original cell ID indicies
         reverse_cellID_idx_lookup_map = cls._build_cellID_reverse_lookup_map(cell_ids)
 
-        return spike_matrix, spike_cells, num_cells, spike_list, spike_positions_list, flat_cell_ids, reverse_cellID_idx_lookup_map
+        return spike_matrix, spike_cells, num_cells, spike_list, spike_positions_list, flat_cell_ids, reverse_cellID_idx_lookup_map, spikes_cell_info_out_dict
 
     # @classmethod
     # def _load_positionAnalysis_mat_file(cls, filepath, session):
