1143dde792747e1d461c2eaa318e08b1fb0ae7d9
nkinsky <nkinsky@umich.edu>
2022-10-12 13:55:50 -0400

    add flatten lists function

--- neuropy/utils/misc.py
+++ neuropy/utils/misc.py
@@ -1,4 +1,5 @@
 import numpy as np
+from collections.abc import Iterable
 
 
 def get_interval(self, period, nwindows):
@@ -6,3 +7,11 @@ def get_interval(self, period, nwindows):
     interval = np.linspace(period[0], period[1], nwindows + 1)
     interval = [[interval[i], interval[i + 1]] for i in range(nwindows)]
     return interval
+
+
+def flatten(xs):
+    for x in xs:
+        if isinstance(x, Iterable) and not isinstance(x, (str, bytes)):
+            yield from flatten(x)
+        else:
+            yield x
