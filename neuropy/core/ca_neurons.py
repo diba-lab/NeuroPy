e52c2c93518b88c7dd6d2b2e03e2f58177fb5727
nkinsky <nkinsky@umich.edu>
2022-10-13 11:19:54 -0400

    Add CaNeuronReg class + plotting functions + bugfix

--- neuropy/core/ca_neurons.py
+++ neuropy/core/ca_neurons.py
@@ -41,23 +41,33 @@ class CaNeurons(DataWriter):
         self.neuron_ids = neuron_ids
         self.neuron_type = neuron_type
 
-    def plot_ROIs(
+    def plot_rois(
         self,
         neuronIDs: list or np.ndarray or None = None,
         label: bool = False,
         ax: plt.Axes or None = None,
-        plot_masks: bool = True,
+        plot_masks: bool = False,
+        plot_max: bool = True,
     ):
+        # Set up axes
         if ax is None:
             fig, ax = plt.subplots()
         elif isinstance(ax, plt.Axes):
             fig = ax.figure
 
+        # Get rois
         neuronIDs = np.arange(self.A.shape[0]) if neuronIDs is None else neuronIDs
         Aplot = self.A[neuronIDs]
+
+        # Plot masks if specified
         if plot_masks:
             ax.imshow((Aplot > 0).sum(axis=0))
 
+        # Plot max projection if specified
+        if plot_max:
+            ax.imshow(self.max_proj)
+
+        # Plot roi outlines with numbers if specified
         if label:
             for id, roi in zip(neuronIDs, Aplot):
                 xedges, yedges = detect_roi_edges(roi > 0)
@@ -98,8 +108,26 @@ class CaNeurons(DataWriter):
             )
 
         # Plot
-        ax.imshow(proj_plot)
+        ax.imshow(proj_plot, cmap="gray")
         ax.invert_yaxis() if flip_yaxis else None  # invert axis if specified
+
+    def plot_rois_with_min_proj(
+        self, plot_max=True, min_type="mc_min_crop", ax=None, label=True, **kwargs
+    ):
+        """plots ROIs over max projection alongside min projection. Great for data QC purposes.
+        **kwargs inputs go to .plot_ROIs"""
+
+        # Set up axes
+        if not isinstance(ax, np.ndarray):
+            _, ax = plt.subplots(1, 2)
+        assert isinstance(ax, np.ndarray) and len(ax) == 2, "ax must be shape (2,)"
+
+        # Plot min
+        self.plot_proj(min_type, ax=ax[0])
+
+        # Plot rois over max
+        self.plot_rois(plot_max=plot_max, ax=ax[1], label=label, **kwargs)
+        pass
 
     def plot_traces(self):
         pass
@@ -108,9 +136,47 @@ class CaNeurons(DataWriter):
         pass
 
 
-def detect_roi_edges(roi_binary):
+class CaNeuronReg:
+    """All things related to tracking neurons across days"""
+
+    def __init__(self, caneurons: list, alias: list or None = None):
+        self.caneurons = caneurons
+        self.alias = alias
+        self.nsessions = len(caneurons)
+        pass
+
+    def get_session(self, sesh_alias: str):
+        """grab a session from its alias"""
+        assert sesh_alias in self.alias, "'sesh_alias' must be in self.alias"
+        sesh_ind = np.where([sesh_alias == sesh for sesh in self.alias])[0][0]
+
+        return self.caneurons[sesh_ind]
+
+    def plot_rois_across_sessions(self, fig_title="", **kwargs):
+        """Plot rois over max proj with min proj also across days.
+        **kwargs to CaNeurons.plot_rois_with_min_proj"""
+        fig, ax = plt.subplots(2, self.nsessions, figsize=(5.67 * self.nsessions, 12.6))
+
+        # Get names for each session - either by session alias or overall session number
+        names = (
+            self.alias
+            if self.alias is not None
+            else [f"Session {n+1}" for n in range(self.nsessions)]
+        )
+
+        for a, caneurons, name in zip(ax.T, self.caneurons, names):
+            caneurons.plot_rois_with_min_proj(ax=a, **kwargs)
+            a[0].set_title(name)
+
+        fig.suptitle(fig_title)
+
+    def register_cells(self):
+        pass
+
+
+def detect_roi_edges(roi_binary, **kwargs):
     """Detect roi edges and organize them nicely in CW/CCW fashion"""
-    edges = feature.canny(roi_binary)  # detect edges
+    edges = feature.canny(roi_binary, **kwargs)  # detect edges
     inds = np.where(edges)  # Get edge locations in pixels
     isort = np.argsort(
         np.arctan2(inds[1] - inds[1].mean(), inds[0] - inds[0].mean())
