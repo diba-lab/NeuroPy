61509946038f76d45e69edb1f00ea54d3970a302
nkinsky <nkinsky@umich.edu>
2022-10-27 10:37:12 -0400

    add useful functions for finding peaks and interpolating nan values in ndarrays

--- neuropy/utils/misc.py
+++ neuropy/utils/misc.py
@@ -1,8 +1,33 @@
 import numpy as np
+import math
 from collections.abc import Iterable
 
 
-def get_interval(self, period, nwindows):
+def find_nearest(array, value):
+    """https://stackoverflow.com/questions/2566412/find-nearest-value-in-numpy-array"""
+    idx = np.searchsorted(array, value, side="left")
+    if idx > 0 and (
+        idx == len(array)
+        or math.fabs(value - array[idx - 1]) < math.fabs(value - array[idx])
+    ):
+        return array[idx - 1]
+    else:
+        return array[idx]
+
+
+def arg_find_nearest(array, value):
+    """https://stackoverflow.com/questions/2566412/find-nearest-value-in-numpy-array"""
+    idx = np.searchsorted(array, value, side="left")
+    if idx > 0 and (
+        idx == len(array)
+        or math.fabs(value - array[idx - 1]) < math.fabs(value - array[idx])
+    ):
+        return idx - 1
+    else:
+        return idx
+
+
+def get_interval(period, nwindows):
 
     interval = np.linspace(period[0], period[1], nwindows + 1)
     interval = [[interval[i], interval[i + 1]] for i in range(nwindows)]
@@ -15,3 +40,36 @@ def flatten(xs):
             yield from flatten(x)
         else:
             yield x
+
+
+def nan_helper(y: np.ndarray):
+    """Helper to handle indices and logical indices of NaNs.
+    From https://stackoverflow.com/questions/6518811/interpolate-nan-values-in-a-numpy-array
+
+    Input:
+        - y, 1d numpy array with possible NaNs
+    Output:
+        - nans, logical indices of NaNs
+        - index, a function, with signature indices= index(logical_indices),
+          to convert logical indices of NaNs to 'equivalent' indices
+    Example:
+        >>> # linear interpolation of NaNs
+        >>> nans, x= nan_helper(y)
+        >>> y[nans]= np.interp(x(nans), x(~nans), y[~nans])
+    """
+
+    return np.isnan(y), lambda z: z.nonzero()[0]
+
+
+def interp_nans(y: np.ndarray):
+    """interpolate nans based on values on either side of them in an array! In case of 2d array
+    will move along rows"""
+
+    if y.ndim == 2:
+        for idr, yrow in enumerate(y):
+            y[idr] = interp_nans(yrow)
+    else:
+
+        nans, x = nan_helper(y)
+        y[nans] = np.interp(x(nans), x(~nans), y[~nans])
+    return y
