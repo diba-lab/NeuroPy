5b7f9cf083926515b1c4354fdf69ebf373ca1e8f
Pho Hale <CommanderPho@users.noreply.github.com>
2023-02-01 21:46:29 -0500

    Restricted to Python<3.11 to allow numba support

--- setup.py
+++ setup.py
@@ -13,7 +13,8 @@ requires = [
     "h5py",
     "hdf5storage",
     "numba",
-    "python-benedict"
+    "python-benedict",
+    "portion"
 ]
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
--- test.py
+++ test.py
@@ -1,1 +1,4 @@
 # This is a test file.
+python tests/test_utils.py
+python tests/test_position.py
+python tests/test_placefields.py
