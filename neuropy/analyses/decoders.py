d1dc9361aea16e1ce0158503ad5b8f94defda33a
nkinsky <nkinsky@umich.edu>
2023-03-22 15:03:01 -0400

    error checking

--- neuropy/analyses/decoders.py
+++ neuropy/analyses/decoders.py
@@ -5,14 +5,11 @@ from scipy import stats
 from tqdm import tqdm
 import scipy.signal as sg
 from typing import Union
-<<<<<<< HEAD
 
 try:
     from numpy.typing import NDArray
 except ModuleNotFoundError:
     print("WARNING: numpy.typing.NDArray can't be imported.")
-=======
->>>>>>> 6f87cf9... import bugfix
 from .placefields import Pf1D, Pf2D
 from .. import core
 from .. import plotting
