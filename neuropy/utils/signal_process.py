b4123128865c9b2f91494f3da9d6ab2198084cba
bapungiri <bapun.k.giri@gmail.com>
2023-06-19 17:16:15 -0400

    moved spectrogram from signal to signal_process

--- neuropy/utils/signal_process.py
+++ neuropy/utils/signal_process.py
@@ -32,6 +32,106 @@ except ImportError:
 from .. import core
 
 
+class Spectrogram(core.Signal):
+    def __init__(self, traces, freqs, sampling_rate=1, t_start=0) -> None:
+        super().__init__(traces, sampling_rate, t_start=t_start, channel_id=freqs)
+
+    @property
+    def freqs(self):
+        return self.channel_id
+
+    def time_slice(self, t_start=None, t_stop=None):
+        return super().time_slice(t_start=t_start, t_stop=t_stop)
+
+    def freq_slice(self, f1=None, f2=None):
+        if f1 is None:
+            f1 = self.freqs[0]
+
+        if f2 is None:
+            f2 = self.freqs[-1]
+
+        assert f1 >= self.freqs[0], "f1 should be greater than lowest frequency"
+        assert (
+            f2 <= self.freqs[-1]
+        ), "f2 should be lower than highest possible frequency"
+        assert f2 > f1, "f2 should be greater than f1"
+
+        ind = np.where((self.freqs >= f1) & (self.freqs <= f2))[0]
+        freqs = self.freqs[ind]
+
+        return Spectrogram(
+            traces=self.traces[ind],
+            freqs=freqs,
+            sampling_rate=self.sampling_rate,
+            t_start=self.t_start,
+        )
+
+    def mean_power(self):
+        return np.mean(self.traces, axis=0)
+
+    def get_band_power(self, f1=None, f2=None):
+        if f1 is None:
+            f1 = self.freqs[0]
+
+        if f2 is None:
+            f2 = self.freqs[-1]
+
+        assert f1 >= self.freqs[0], "f1 should be greater than lowest frequency"
+        assert (
+            f2 <= self.freqs[-1]
+        ), "f2 should be lower than highest possible frequency"
+        assert f2 > f1, "f2 should be greater than f1"
+
+        ind = np.where((self.freqs >= f1) & (self.freqs <= f2))[0]
+        band_power = np.mean(self.traces[ind, :], axis=0)
+        return band_power
+
+    def get_band_ratio(self, band1, band2):
+        assert len(band1) == 2, "band1 must have 2 frequencies"
+        assert len(band2) == 2, "band2 must have 2 frequencies"
+        assert np.diff(band1)[0] > 0, "band1: 2nd value should be greater"
+        assert np.diff(band2)[0] > 0, "band2: 2nd value should be greater"
+
+        return self.get_band_power(*band1) / self.get_band_power(*band2)
+
+    @property
+    def delta(self):
+        return self.get_band_power(f1=0.5, f2=4)
+
+    @property
+    def deltaplus(self):
+        deltaplus_ind = np.where(
+            ((self.freqs > 0.5) & (self.freqs < 4))
+            | ((self.freqs > 12) & (self.freqs < 15))
+        )[0]
+        deltaplus_sxx = np.mean(self.traces[deltaplus_ind, :], axis=0)
+        return deltaplus_sxx
+
+    @property
+    def theta(self):
+        return self.get_band_power(f1=5, f2=10)
+
+    @property
+    def spindle(self):
+        return self.get_band_power(f1=10, f2=20)
+
+    @property
+    def gamma(self):
+        return self.get_band_power(f1=30, f2=90)
+
+    @property
+    def ripple(self):
+        return self.get_band_power(f1=140, f2=250)
+
+    @property
+    def theta_delta_ratio(self):
+        return self.theta / self.delta
+
+    @property
+    def theta_deltaplus_ratio(self):
+        return self.theta / self.deltaplus
+
+
 class filter_sig:
     @staticmethod
     def bandpass(signal, lf, hf, fs=1250, order=3, ax=-1):
@@ -157,7 +257,7 @@ def whiten(strain, interp_psd, dt):
     return white_ht
 
 
-class WaveletSg(core.Spectrogram):
+class WaveletSg(Spectrogram):
     def __init__(
         self,
         signal: core.Signal,
@@ -229,7 +329,7 @@ class WaveletSg(core.Spectrogram):
         return np.abs(conv_val).astype("float32")
 
 
-class FourierSg(core.Spectrogram):
+class FourierSg(Spectrogram):
     def __init__(
         self,
         signal: core.Signal,
