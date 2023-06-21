eea61da2b6ef878bc4402ebd8348ce2635e8666a
bapungiri <bapun.k.giri@gmail.com>
2023-06-21 11:49:36 -0400

    import fixes

--- neuropy/core/__init__.py
+++ neuropy/core/__init__.py
@@ -3,6 +3,6 @@ from .position import Position
 from .datawriter import DataWriter
 from .neurons import Neurons, BinnedSpiketrain, Mua
 from .probe import Shank, Probe, ProbeGroup
-from .signal import Signal, Spectrogram
+from .signal import Signal
 from .ratemap import Ratemap
 from .animal import Animal
