d8fbe067a325dfe4f92c58e01b5e88e85e23aca2
Pho Hale <PhoHale@gmail.com>
2023-06-21 14:15:23 -0400

    added `StringLiteralComparableEnum` and tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -22,7 +22,13 @@ except ModuleNotFoundError as e:
 finally:
     from neuropy.utils.misc import is_iterable
     from neuropy.utils.result_context import IdentifyingContext
-
+    from neuropy.utils.mixins.enum_helpers import StringLiteralComparableEnum
+    
+# Define your custom enum type:
+class UnitColoringMode(StringLiteralComparableEnum):
+    PRESERVE_FRAGILE_LINEAR_NEURON_IDXS = "preserve_fragile_linear_neuron_IDXs"
+    COLOR_BY_INDEX_ORDER = "color_by_index_order"
+    
 
 class TestUtilityMethods(unittest.TestCase):
 
@@ -40,7 +46,21 @@ class TestUtilityMethods(unittest.TestCase):
         self.assertTrue(is_iterable(np.array([0, 1, 2])), "ndArray should return true for is_iterable")
         self.assertTrue(is_iterable(pd.DataFrame({'col':[0, 1, 2]})), "Pandas DataFrame should return true for is_iterable")
         
-        
+
+
+    def test_string_literal_comparable_enum(self):
+        test1 = UnitColoringMode.PRESERVE_FRAGILE_LINEAR_NEURON_IDXS
+        test2 = UnitColoringMode.COLOR_BY_INDEX_ORDER
+
+        self.assertIsInstance(test1.value, str, "Value of enum must be string")
+        self.assertEqual(test1.value, "preserve_fragile_linear_neuron_IDXs")
+        self.assertNotEqual(test1, test2)
+        self.assertEqual(test1, "preserve_fragile_linear_neuron_IDXs")
+        self.assertEqual(test1, "preserve_fragile_linear_NEURON_IDXs")
+        self.assertNotEqual(test1, "color_by_index_order") # compare to wrong value
+        self.assertNotEqual(test1, "a_fake_value") # compare to fake value
+
+
 
     def test_progress_message_printer(self):
         # TODO: not sure how to test this
