a98956659aef2fcbd5a08c9c96c854d99b811de2
Pho Hale <PhoHale@gmail.com>
2023-08-23 19:08:53 -0400

    fixed spacing

--- neuropy/core/neurons.py
+++ neuropy/core/neurons.py
@@ -28,11 +28,11 @@ from neuropy.core.neuron_identities import NeuronIdentityTable, neuronTypesList,
 @total_ordering
 @unique
 class NeuronType(HDF_Converter.HDFConvertableEnum, Enum):
-    """ 
+    """
     Kamran 2023-07-18:
         cluq=[1,2,4,9] all passed.
         3 were noisy
-        [6,7]: double fields. 
+        [6,7]: double fields.
         5: interneurons
 
     Pho-Pre-2023-07-18:
@@ -43,31 +43,31 @@ class NeuronType(HDF_Converter.HDFConvertableEnum, Enum):
     PYRAMIDAL = 0
     CONTAMINATED = 1
     INTERNEURONS = 2
-    
-    # [name for name, member in NeuronType.__members__.items() if member.name != name]    
+
+    # [name for name, member in NeuronType.__members__.items() if member.name != name]
     # longClassNames = ['pyramidal','contaminated','interneurons']
     # shortClassNames = ['pyr','cont','intr']
     # classCutoffValues = [0, 4, 7, 9]
 
     def describe(self):
         self.name, self.value
-    
+
     # def __repr__(self) -> str:
     #     return super().__repr__()
-    
+
     def __eq__(self, other):
         return self.value == other.value
-    
+
     def __le__(self, other):
         return self.value < other.value
-    
+
     def __hash__(self):
         return hash(self.value)
-    
+
     @property
     def shortClassName(self):
         return NeuronType.shortClassNames()[self.value]
-        
+
     @property
     def longClassName(self):
         return NeuronType.longClassNames()[self.value]
@@ -84,11 +84,11 @@ class NeuronType(HDF_Converter.HDFConvertableEnum, Enum):
     @classmethod
     def longClassNames(cls):
         return np.array(['pyramidal','contaminated','interneurons'])
-    
+
     @classmethod
     def shortClassNames(cls):
         return np.array(['pyr','cont','intr'])
-    
+
     @classmethod
     def bapunNpyFileStyleShortClassNames(cls):
         return np.array(['pyr','mua','inter'])
@@ -96,12 +96,12 @@ class NeuronType(HDF_Converter.HDFConvertableEnum, Enum):
     @classmethod
     def hdf_coding_ClassNames(cls):
         return np.array(['pyr','bad','intr'])
-    
+
     @classmethod
     def classCutoffValues(cls):
         raise NotImplementedError(f"this method has been depricated in favor of cls.classCutoffMap after it was revealed by Kamran that the qclu values are non-sequential on 2023-07-18.")
         # return np.array([0, 4, 7, 9])
-    
+
 
     @classmethod
     def classCutoffMap(cls) -> dict:
@@ -109,22 +109,22 @@ class NeuronType(HDF_Converter.HDFConvertableEnum, Enum):
             Kamran 2023-07-18:
                 cluq=[1,2,4,9] all passed.
                 3 were noisy
-                [6,7]: double fields. 
+                [6,7]: double fields.
                 5: interneurons
-                
-        ## Post 2023-07-18: 
-            for i in np.arange(10): 
+
+        ## Post 2023-07-18:
+            for i in np.arange(10):
                 _out_map[i] = "cont" # initialize all to 'contaminated'/noisy
             for i in [1,2,4,6,7,9]:
                 _out_map[i] = 'pyr' # pyramidal
             _out_map[5] = "intr" # interneurons
-        
+
         ## Post 2023-07-31 - Excluding "double fields" qclues on Kamran's request
-            
+
 
         """
         _out_map = dict() # start with an empty dict
-        for i in np.arange(10): 
+        for i in np.arange(10):
             _out_map[i] = "cont" # initialize all to 'contaminated'/noisy
         for i in [1,2,4,6,7,9]:
             _out_map[i] = 'pyr' # pyramidal
@@ -138,13 +138,13 @@ class NeuronType(HDF_Converter.HDFConvertableEnum, Enum):
         string_value = string_value.lower()
         itemindex = np.where(cls.shortClassNames()==string_value)
         return NeuronType(itemindex[0])
-    
+
     @classmethod
     def from_long_string(cls, string_value) -> "NeuronType":
         string_value = string_value.lower()
         itemindex = np.where(cls.longClassNames()==string_value)
-        return NeuronType(itemindex[0])    
-    
+        return NeuronType(itemindex[0])
+
     @classmethod
     def from_string(cls, string_value) -> "NeuronType":
         string_value = string_value.lower()
@@ -160,7 +160,7 @@ class NeuronType(HDF_Converter.HDFConvertableEnum, Enum):
                     itemindex = np.where(cls.hdf_coding_ClassNames()==string_value)
 
         return NeuronType(itemindex[0])
-        
+
     @classmethod
     def from_bapun_npy_style_string(cls, string_value) -> "NeuronType":
         string_value = string_value.lower()
@@ -169,8 +169,8 @@ class NeuronType(HDF_Converter.HDFConvertableEnum, Enum):
 
 
 
-    
-    
+
+
     @classmethod
     def from_qclu_series(cls, qclu_Series):
         # qclu_Series: a Pandas Series object, such as qclu_Series=spikes_df['qclu']
@@ -180,13 +180,13 @@ class NeuronType(HDF_Converter.HDFConvertableEnum, Enum):
         temp_neuronTypeStrings = [temp_cutoff_map[int(qclu)] for qclu in qclu_Series]
         temp_neuronTypes = np.array([NeuronType.from_short_string(_) for _ in np.array(temp_neuronTypeStrings)])
         return temp_neuronTypes
-        
+
     @classmethod
     def from_any_string_series(cls, neuron_types_strings):
         # neuron_types_strings: a np.ndarray containing any acceptable style strings, such as: ['mua', 'mua', 'inter', 'pyr', ...]
         return np.array([NeuronType.from_string(_) for _ in np.array(neuron_types_strings)])
-    
-    
+
+
     @classmethod
     def from_bapun_npy_style_series(cls, bapun_style_neuron_types):
         # bapun_style_neuron_types: a np.ndarray containing Bapun-style strings, such as: ['mua', 'mua', 'inter', 'pyr', ...]
@@ -207,7 +207,7 @@ class NeuronType(HDF_Converter.HDFConvertableEnum, Enum):
     @property
     def renderColor(self):
         return NeuronType.classRenderColors()[self.value]
-  
+
 
     # HDFConvertableEnum Conformances ____________________________________________________________________________________ #
     @classmethod
@@ -217,14 +217,14 @@ class NeuronType(HDF_Converter.HDFConvertableEnum, Enum):
     @classmethod
     def convert_to_hdf(cls, value) -> str:
         return value.hdfcodingClassName
-    
+
     @classmethod
     def from_hdf_coding_string(cls, string_value: str) -> "NeuronType":
         string_value = string_value.lower()
         itemindex = np.where(cls.hdf_coding_ClassNames()==string_value)
         return NeuronType(itemindex[0])
 
-        
+
 
 
 class Neurons(HDF_SerializationMixin, NeuronUnitSlicableObjectProtocol, StartStopTimesMixin, TimeSlicableObjectProtocol, ConcatenationInitializable, DataWriter):
