6b5af9fa9e2123f69fa91454bd5d83a60e0668ac
nkinsky <nkinsky@umich.edu>
2023-10-19 12:32:34 -0400

    add function outlines - no code yet

--- neuropy/core/sync.py
+++ neuropy/core/sync.py
@@ -50,3 +50,23 @@ class OESync(DataWriter):
         self.basepath = Path(basepath)
         self.sess = ProcessData(basepath)
         self.oe_sync_df = oeio.create_sync_df(self.basepath)
+
+    def check_external_ts_vs_ttl(self, external_ts, ttl_num, plot_check=True):
+        """Checks external timestamps vs corresponding TTLs in OpenEphys to make sure there is a consistent lag between
+        expected TTL time and actual TTL time.  Raise a flag/warning if that is not the case or if there is a dropped
+        TTL somewhere
+        :param external_ts: timezone aware array of timestamps (OR array of elapsed times from start of recording?),
+        calculated by an external (un-synced) system
+        :param ttl_num: int for OE TTL channel corresponding to all timestamps in external_ts
+        :param plot_check: bool, plots lags between TTLs versus time and identifies dropped frames
+
+        :return lags: np.ndarray of lags to correct external_ts to combined time
+        :return good_bool: boolean np.ndarray of frames with corresponding TTLs in OpenEphys files
+        """
+        pass
+
+    def sync_external_ts_to_oe(self, external_ts, ttl_num, plot_check):
+        """Matches up external timestamps to TTL times in OE. See check_external_ts_vs_ttl
+        :return: np.ndarray of external timestamps which correspond to OE TTLs in combined OE time
+        :return: good_bool: boolean np.ndarray of frames with corresponding TTLs in OpenEphys files.
+                            external_ts[good_bool] will return times of all frames recorded in OE."""
