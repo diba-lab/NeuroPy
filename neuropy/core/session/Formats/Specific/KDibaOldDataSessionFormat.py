6eb54710436a7cf06c759f6b91857fafa5877872
Pho Hale <PhoHale@gmail.com>
2024-01-24 13:53:02 -0500

    made note about how I can call `session.laps.update_maze_id_if_needed()` if I can get the times from the session

--- neuropy/core/session/Formats/Specific/KDibaOldDataSessionFormat.py
+++ neuropy/core/session/Formats/Specific/KDibaOldDataSessionFormat.py
@@ -451,6 +451,8 @@ class KDibaOldDataSessionFormatRegisteredClass(DataSessionFormatBaseRegisteredCl
             print('session.laps loaded successfully!')
             pass
         
+        # session.laps.update_maze_id_if_needed( # could do this here if I had the t_start, t_delta, and t_split
+        # if session.epochs
 
         ## Replays:
         try:
@@ -888,7 +890,6 @@ class KDibaOldDataSessionFormatRegisteredClass(DataSessionFormatBaseRegisteredCl
         # session.laps = Laps(lap_id, laps_spike_counts, lap_start_stop_flat_idx, lap_start_stop_time)
         
         session.laps.update_lap_dir_from_smoothed_velocity(session) # added 2024-01-24 
-        # session.laps.update_maze_id_if_needed(
 
         # return lap_id, laps_spike_counts, lap_start_stop_flat_idx, lap_start_stop_time
         return session, spikes_df
