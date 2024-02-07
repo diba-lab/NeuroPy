cd9ba83c2735bb5b6776bf72ba36550949df272a
Pho Hale <PhoHale@gmail.com>
2024-02-07 13:06:56 -0500

    added type_aliases

---
+++ neuropy/utils/type_aliases.py
@@ -0,0 +1,14 @@
+from typing import Dict, List, Tuple, Optional, Callable, Union, Any
+from typing_extensions import TypeAlias  # "from typing_extensions" in Python 3.9 and earlier
+from nptyping import NDArray
+
+""" Usage:
+
+from typing import Dict, List, Tuple, Optional, Callable, Union, Any
+from typing_extensions import TypeAlias
+from nptyping import NDArray
+import neuropy.utils.type_aliases as types
+
+"""
+
+aclu_index: TypeAlias = int # an integer index that is an aclu
