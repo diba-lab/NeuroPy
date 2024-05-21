aa5393a4cb7c127e3e27719cd6f9f66440e20dc7
Pho Hale <PhoHale@gmail.com>
2024-05-21 19:03:01 -0400

    added `DecoderName` NewType to neuropy.types

--- neuropy/utils/type_aliases.py
+++ neuropy/utils/type_aliases.py
@@ -1,5 +1,6 @@
 from typing import Dict, List, Tuple, Optional, Callable, Union, Any
 from typing_extensions import TypeAlias  # "from typing_extensions" in Python 3.9 and earlier
+from typing import NewType
 from nptyping import NDArray
 
 """ Usage:
@@ -12,3 +13,5 @@ import neuropy.utils.type_aliases as types
 """
 
 aclu_index: TypeAlias = int # an integer index that is an aclu
+DecoderName = NewType('DecoderName', str)
+
